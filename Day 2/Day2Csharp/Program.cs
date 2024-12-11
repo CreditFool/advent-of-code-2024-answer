8d86b9fd3ada7dd1c5d9e4867a5d8e6109ad29d9
Ichlalsul Bulqiah aka creditFool <bichlasulbulqiah@gmail.com>
2024-12-11 15:33:00 +0700

    create day 2 implementation with C#

---
+++ Day 2/Day2Csharp/Program.cs
@@ -0,0 +1,64 @@
+﻿static void CountSafeReport()
+{
+    try
+    {
+        int result = 0;
+        int resultWithTolerant = 0;
+
+        using StreamReader sr = new(File.Open("/home/enigma/kodingan/project/advent-of-code-2024-code/Day 2/input.txt", FileMode.Open));
+        String? line = sr.ReadLine();
+        while (line != null && line != string.Empty)
+        {
+            if (IsSafe(line)) result++;
+            if (IsSafe(line, 1)) resultWithTolerant++;
+            line = sr.ReadLine();
+        }
+
+        Console.WriteLine($"Safe report: {result}");
+        Console.WriteLine($"Safe report with 1 tolerant: {resultWithTolerant}");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        throw;
+    }
+
+}
+
+static bool IsSafe(string line, int tolerant = 0)
+{
+    bool? isAscend = null;
+    int badLevel = 0;
+    string[] levels = line.Split(" ");
+    for (int i = 0; i < levels.Length - 1; i++)
+    {
+        int currLevel = int.Parse(levels[i]);
+        int nextLevel = int.Parse(levels[i + 1]);
+        int diff = Math.Abs(currLevel - nextLevel);
+
+        if (isAscend == null)
+        {
+            if (diff < 1 || diff > 3)
+            {
+                badLevel++;
+            }
+            isAscend = currLevel < nextLevel;
+        }
+        else if (((bool)isAscend) && ((currLevel > nextLevel) || diff < 1 || diff > 3))
+        {
+            badLevel++;
+        }
+        else if ((!(bool)isAscend) && ((currLevel < nextLevel) || diff < 1 || diff > 3))
+        {
+            badLevel++;
+        }
+
+        if (badLevel > tolerant)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+CountSafeReport();
